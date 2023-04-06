00472e9ff756d5abc679d78525b1ba43b49990af
Collin Chiu <collinchiu23@gmail.com>
2023-04-05 22:45:50 -0700

    Implement Test class

--- 001_twoSum/twoSum.py
+++ 001_twoSum/twoSum.py
@@ -11,23 +11,28 @@ class Solution:
             d[j] = i
 
 
-# Testing
-test_cases = [
-    ([2, 7, 11, 15], 9, [0, 1]),
-    ([3, 2, 4], 6, [1, 2]),
-    ([3, 3], 6, [0, 1]),
-    ([4, 7, 10, 2], 6, [0, 3]),
-    ([5, 10, -5], 0, [0, 1]),
-]
+class Test:
+    def __init__(self):
+        self.test_cases = [
+            ([2, 7, 11, 15], 9, [0, 1]),
+            ([3, 2, 4], 6, [1, 2]),
+            ([3, 3], 6, [0, 1]),
+            ([4, 7, 10, 2], 6, [0, 3]),
+            ([5, 10, -5], 0, [0, 2]),
+        ]
 
-for nums, target, ans in test_cases:
-    result = Solution().twoSum(nums, target)
-    print(
-        f"""Input: nums = {nums}, target = {target}
-            Output: {result}
-            Expected Output: {ans}"""
-    )
-    if result == ans:
-        print("Correct output")
-    else:
-        print("Incorrect output")
+    def run_tests(self):
+        for nums, target, ans in self.test_cases:
+            result = Solution().twoSum(nums, target)
+            print(
+                f"""Input: nums = {nums}, target = {target}
+                Output: {result}
+                Expected Output: {ans}"""
+            )
+            if result == ans:
+                print("Correct output")
+            else:
+                print("Incorrect output")
+
+
+Test().run_tests()
