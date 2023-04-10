dc9c06466e2fd5e374866e20dbdcfa6d21edbb50
Collin Chiu <collinchiu23@gmail.com>
2023-04-09 21:32:20 -0700

    Add pseudocode to twoSum

--- 001_twoSum/twoSum.py
+++ 001_twoSum/twoSum.py
@@ -9,3 +9,12 @@ class Solution:
             if a in d:
                 return [d[a], i]
             d[j] = i
+
+
+# Pseudocode:
+# create an empty dictionary
+# loop through nums list: keep track of i (0-n) and j (0-n values)
+#   calculate remainder a: target - value
+#   if a is in dict:
+#       return array of [a value, current index i]
+#   if not in array, append value(key) and index (value) into dict
