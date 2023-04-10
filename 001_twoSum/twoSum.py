24322ff9e97a94525062a0da7e398042318f67f9
Collin Chiu <collinchiu23@gmail.com>
2023-04-09 21:19:19 -0700

    Remove Test class

--- 001_twoSum/twoSum.py
+++ 001_twoSum/twoSum.py
@@ -9,30 +9,3 @@ class Solution:
             if a in d:
                 return [d[a], i]
             d[j] = i
-
-
-class Test:
-    def __init__(self):
-        self.test_cases = [
-            ([2, 7, 11, 15], 9, [0, 1]),
-            ([3, 2, 4], 6, [1, 2]),
-            ([3, 3], 6, [0, 1]),
-            ([4, 7, 10, 2], 6, [0, 3]),
-            ([5, 10, -5], 0, [0, 2]),
-        ]
-
-    def run_tests(self):
-        for nums, target, ans in self.test_cases:
-            result = Solution().twoSum(nums, target)
-            print(
-                f"""Input: nums = {nums}, target = {target}
-                Output: {result}
-                Expected Output: {ans}"""
-            )
-            if result == ans:
-                print("Correct output")
-            else:
-                print("Incorrect output")
-
-
-Test().run_tests()
